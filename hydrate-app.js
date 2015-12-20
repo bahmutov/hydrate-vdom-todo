8cc4796b4193fcfe6d082387276d52a1f6c372ef
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-20 14:02:05 -0500

    saving hydrated file

--- hydrate-app.js
+++ hydrate-app.js
@@ -2,7 +2,7 @@ const toHTML = require('vdom-to-html')
 const h = require('virtual-dom/h')
 
 const read = require('fs').readFileSync
-const index = read('./index.html')
+const index = read('./index.html', 'utf-8')
 
 const todos = require('./src/data')
 console.log('initial data has %d todo(s)', todos.length)
@@ -11,4 +11,15 @@ const renderTodos = require('./src/render-todos')
 const rendered = renderTodos(todos)
 
 const beautify = require('js-beautify').html
-console.log(beautify(toHTML(rendered), { indent_size: 2 }))
+const appMarkup = beautify(toHTML(rendered), { indent_size: 2 });
+console.log(appMarkup)
+
+const outputIndexFilename = './dist/index.html'
+const write = require('fs').writeFileSync
+
+const appId = '<div id="app">'
+const appDiv = appId + '</div>'
+const hydratedIndex = index.replace(appDiv, appId + '\n' + appMarkup + '\n' + '</div>')
+
+write(outputIndexFilename, hydratedIndex)
+console.log('saved hydrated index', outputIndexFilename)
